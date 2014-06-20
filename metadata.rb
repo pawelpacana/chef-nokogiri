9ded10107bd632e62917d4ae5409cc066c07fab0
Paweł Pacana <pawel.pacana@syswise.eu>
2014-06-20 11:57:50 +0200

    Add missing name metadata.

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,5 @@ maintainer_email "blake@hilscher.ca"
 license          "All rights reserved"
 description      "Cookbook for installing nokogiri depedencies."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
+name             "nokogiri"
